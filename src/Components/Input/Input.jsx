a00f5c4d6837d44fba5701264756d6d2cb0b37db
Alexey Babenko <alexey22042001@gmail.com>
2023-02-20 16:49:09 +0200

    add forwardRef; add onSelect prop

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,32 +1,68 @@
+import { forwardRef } from 'react'
 import style from './Input.module.scss'
 
-function Input({
-  type = 'text',
-  id,
-  name,
-  placeholder = '',
-  value = '',
-  required = false,
-  onChange,
-  className = '',
-  style: inlineStyle = {},
-}) {
+const Input = forwardRef(function Input(
+  {
+    type = 'text',
+    id,
+    name,
+    placeholder = '',
+    value = '',
+    required = false,
+    onChange,
+    onSelect,
+    className = '',
+    style: inlineStyle = {},
+  },
+  ref
+) {
   return (
     <div
       className={`${className} ${style.input}`}
       style={inlineStyle}
     >
       <input
+        ref={ref}
         type={type}
         id={id}
         name={name}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        onSelect={onSelect}
         required={required}
       />
     </div>
   )
-}
+})
+
+// function Input({
+//   type = 'text',
+//   id,
+//   name,
+//   placeholder = '',
+//   value = '',
+//   required = false,
+//   onChange,
+//   className = '',
+//   style: inlineStyle = {},
+// }) {
+//   return (
+//     <div
+//       className={`${className} ${style.input}`}
+//       style={inlineStyle}
+//     >
+//       <input
+//         type={type}
+//         id={id}
+//         name={name}
+//         placeholder={placeholder}
+//         value={value}
+//         onChange={onChange}
+//         required={required}
+//       />
+//     </div>
+//   )
+// }
 
 export default Input
