9bacc894e4dc2906e567e984357e5cd2f3881926
Alexey Babenko <alexey22042001@gmail.com>
2023-02-22 01:02:57 +0200

    change the way in which the content of the component is generated

--- src/Components/Form/components/FormControl/FormControl.jsx
+++ src/Components/Form/components/FormControl/FormControl.jsx
@@ -1,103 +1,144 @@
-import PropTypes from 'prop-types'
+import { forwardRef, useState } from 'react'
 
 import style from './FormControl.module.scss'
-
-import Input from '../../../Input/Input'
-import { useState } from 'react'
-
-const INPUT_CONFIG_TEMPLATE = {
-  id: crypto.randomUUID,
-  type: 'text',
-  placeholder: '',
-  value: '',
+const STATUS_STYLE = {
+  error: style.danger,
 }
 
-function FormControl({
-  label,
-  inputsConfig = INPUT_CONFIG_TEMPLATE,
-  require = false,
-  style: inlineStyle = {},
-  className = '',
-}) {
-  if (!Array.isArray(inputsConfig)) {
-    inputsConfig = [inputsConfig]
-  }
+import Input from '../../../Input/Input'
 
-  inputsConfig = getProcessedInputsConfig(inputsConfig)
-
-  const [inputsValue, setInputsValue] = useState(
-    inputsConfig.map((inputConfig) => inputConfig.value)
-  )
-
-  const formControlContent =
-    getFormControlContent(inputsConfig)
+const FormControl = forwardRef(function FormControl(
+  {
+    type = 'text',
+    id,
+    name,
+    placeholder,
+    value,
+    onChange,
+    label,
+    required = false,
+    infoMessage,
+    style: inlineStyle = {},
+    className = '',
+  },
+  ref
+) {
+  const formControlContent = getFormControlContent({
+    ref,
+    type,
+    id,
+    name,
+    placeholder,
+    value,
+    onChange,
+    required,
+  })
 
   return (
     <div
       style={inlineStyle}
-      className={`${className} ${style.formControl}`}
+      className={`${className} ${style.formControl} ${
+        STATUS_STYLE[infoMessage?.status]
+      }`}
     >
-      <label
-        htmlFor={inputsConfig[0].id}
-        className={style.label}
-      >
+      <label htmlFor={null} className={style.label}>
         {label}
       </label>
       {formControlContent}
-      <div className={style.infoMessage}></div>
+      <div className={style.infoMessage}>
+        {infoMessage?.message}
+      </div>
+    </div>
+  )
+})
+
+export default FormControl
+
+function getFormControlContent(inputsConfig) {
+  const inputsProps = getInputsProps(inputsConfig)
+
+  if (inputsProps.length === 1) {
+    const props = inputsProps[0]
+
+    return (
+      <Input
+        key={props.get('id')}
+        type={props.get('type')}
+        id={props.get('id')}
+        name={props.get('name')}
+        placeholder={props.get('placeholder')}
+        value={props.get('value')}
+        onChange={props.get('onChange')}
+        required={props.get('required')}
+      />
+    )
+  }
+
+  return (
+    <div className={style.inputsWrapper}>
+      {inputsProps.map((inputProps) => (
+        <Input
+          key={inputProps.get('id')}
+          type={inputProps.get('type')}
+          id={inputProps.get('id')}
+          name={inputProps.get('name')}
+          placeholder={inputProps.get('placeholder')}
+          value={inputProps.get('value')}
+          onChange={inputProps.get('onChange')}
+          required={inputProps.get('required')}
+        />
+      ))}
     </div>
   )
 }
 
-export default FormControl
+function getInputsProps(inputsConfig) {
+  if (!(inputsConfig instanceof Object)) {
+    throw new TypeError(
+      'inputsConfig must be an Object or Map'
+    )
+  }
 
-FormControl.propTypes = {
-  inputsConfig: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.object,
-  ]),
-}
+  if (!(inputsConfig instanceof Map)) {
+    inputsConfig = new Map(Object.entries(inputsConfig))
+  }
 
-function getProcessedInputsConfig(inputsConfig) {
-  return inputsConfig.map((inputConfig) => {
-    const isConfigObject =
-      typeof inputConfig === 'object' &&
-      !Array.isArray(inputConfig) &&
-      inputConfig !== null
+  let inputsProps = null
+  let defaultProps = new Map([['id', crypto.randomUUID()]])
 
-    if (!isConfigObject) {
-      throw new TypeError(
-        `inputsConfig property must be an object or an array with objects: ${inputConfig}`
+  for (let [propName, propValue] of inputsConfig) {
+    if (!Array.isArray(propValue)) {
+      defaultProps.set(propName, propValue)
+      continue
+    }
+
+    if (inputsProps === null) {
+      inputsProps = new Array(propValue.length)
+
+      for (let i = 0; i < inputsProps.length; i++) {
+        inputsProps[i] = new Map()
+      }
+    } else if (inputsProps.length !== propValue.length) {
+      throw new Error(
+        `Inputs' properties must be the same length`
       )
     }
 
-    return {
-      ...INPUT_CONFIG_TEMPLATE,
-      ...inputConfig,
+    for (let i = 0; i < inputsProps.length; i++) {
+      inputsProps[i].set(propName, propValue[i])
     }
-  })
-}
+  }
 
-function getFormControlContent(inputsConfig) {
-  const inputElements = inputsConfig.map((inputConfig) => (
-    <Input
-      key={inputConfig.id}
-      type={inputConfig.type}
-      id={inputConfig.id}
-      name={inputConfig.name}
-      placeholder={inputConfig.placeholder}
-      value={inputConfig.value}
-    />
-  ))
+  if (inputsProps === null) {
+    return [defaultProps]
+  }
 
-  const formControlContent =
-    inputsConfig.length > 1 ? (
-      <div className={style.inputsWrapper /* TODO */}>
-        {inputElements}
-      </div>
-    ) : (
-      inputElements
-    )
+  for (let i = 0; i < inputsProps.length; i++) {
+    inputsProps[i] = new Map([
+      ...defaultProps,
+      ...inputsProps[i],
+    ])
+  }
 
-  return formControlContent
+  return inputsProps
 }
