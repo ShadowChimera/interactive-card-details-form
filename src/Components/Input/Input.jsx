4662bf5ab231e96a01816a2fa94224e6def394ee
Alexey Babenko <alexey22042001@gmail.com>
2023-02-22 02:26:36 +0200

    collect input's props to an object

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -2,18 +2,7 @@ import { forwardRef } from 'react'
 import style from './Input.module.scss'
 
 const Input = forwardRef(function Input(
-  {
-    type = 'text',
-    id,
-    name,
-    placeholder = '',
-    value = '',
-    required = false,
-    onChange,
-    onSelect,
-    className = '',
-    style: inlineStyle = {},
-  },
+  { className = '', style: inlineStyle = {}, ...props },
   ref
 ) {
   return (
@@ -21,48 +10,9 @@ const Input = forwardRef(function Input(
       className={`${className} ${style.input}`}
       style={inlineStyle}
     >
-      <input
-        ref={ref}
-        type={type}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onSelect={onSelect}
-        required={required}
-      />
+      <input ref={ref} {...props} />
     </div>
   )
 })
-
-// function Input({
-//   type = 'text',
-//   id,
-//   name,
-//   placeholder = '',
-//   value = '',
-//   required = false,
-//   onChange,
-//   className = '',
-//   style: inlineStyle = {},
-// }) {
-//   return (
-//     <div
-//       className={`${className} ${style.input}`}
-//       style={inlineStyle}
-//     >
-//       <input
-//         type={type}
-//         id={id}
-//         name={name}
-//         placeholder={placeholder}
-//         value={value}
-//         onChange={onChange}
-//         required={required}
-//       />
-//     </div>
-//   )
-// }
 
 export default Input
