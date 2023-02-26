827dbbc2ad57e75ffdcc31b984a1f30449735549
Alexey Babenko <alexey22042001@gmail.com>
2023-02-26 18:42:09 +0200

    add FormContent component; add form submission handling

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,4 +1,11 @@
-import { useReducer, useRef, useState } from 'react'
+import {
+  memo,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from 'react'
 import style from './Form.module.scss'
 
 import Button from '../Button/Button'
@@ -9,7 +16,6 @@ import formReducer from './formReducer'
 
 function Form({ style: inlineStyle, className = '' }) {
   const [state, dispatch] = useReducer(formReducer, {
-    // isDisabled: false, // TODO make form status
     status: 'default',
     data: {
       cardholderName: {
@@ -30,42 +36,108 @@ function Form({ style: inlineStyle, className = '' }) {
     },
   })
 
-  // const inputRefs = useRef(new Map())
-
   function handleSubmit(e) {
     e.preventDefault()
-
-    if (
-      state.status === 'validating' ||
-      state.status === 'sending'
-    ) {
-      return
-    }
 
     dispatch({
       type: 'submitted_form',
     })
   }
 
+  /* -------------------------------------------------------------------------- */
+  /*                               FORM SUBMITTING                              */
+  /* -------------------------------------------------------------------------- */
+
+  useEffect(() => {
+    if (state.status !== 'sending') {
+      return
+    }
+
+    console.log('sending...')
+    let timerId = setTimeout(
+      () => console.log('success!'),
+      1000
+    )
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [state.status])
+
+  return (
+    <form
+      style={inlineStyle}
+      className={`${className} ${style.form}`}
+      onSubmit={handleSubmit}
+    >
+      <FormContent
+        data={state.data}
+        dispatch={dispatch}
+        disabled={
+          state.status === 'sending' ||
+          state.status === 'success'
+        }
+      />
+      <Button
+        className={style.button}
+        text={
+          state.status === 'sending' ? (
+            <LoadingIcon />
+          ) : (
+            'Confirm'
+          )
+        }
+      />
+    </form>
+  )
+}
+
+export default Form
+
+/* -------------------------------------------------------------------------- */
+/*                                FORM CONTENT                                */
+/* -------------------------------------------------------------------------- */
+
+const FormContent = memo(function FormContent({
+  data,
+  dispatch,
+  disabled = false,
+}) {
   function handleCardholderNameChange(e) {
+    if (disabled) {
+      return
+    }
+
     dispatch({
       type: 'changed_cardholder_name',
       value: e.target.value,
     })
   }
   function handleExpDateMonthChange(e) {
+    if (disabled) {
+      return
+    }
+
     dispatch({
       type: 'changed_exp_date_month',
       value: e.target.value,
     })
   }
   function handleExpDateYearChange(e) {
+    if (disabled) {
+      return
+    }
+
     dispatch({
       type: 'changed_exp_date_year',
       value: e.target.value,
     })
   }
   function handleCvcChange(e) {
+    if (disabled) {
+      return
+    }
+
     dispatch({
       type: 'changed_cvc',
       value: e.target.value,
@@ -75,55 +147,42 @@ function Form({ style: inlineStyle, className = '' }) {
   function handleCardholderNameBlur(e) {
     dispatch({
       type: 'removed_focus_from_cardholder_name',
-      value: e.target.value,
     })
   }
   function handleCardNumberBlur(e) {
     dispatch({
       type: 'removed_focus_from_card_number',
-      value: e.target.value,
     })
   }
   function handleExpDateMonthBlur(e) {
     dispatch({
       type: 'removed_focus_from_exp_date_month',
-      value: e.target.value,
     })
   }
   function handleExpDateYearBlur(e) {
     dispatch({
       type: 'removed_focus_from_exp_date_year',
-      value: e.target.value,
     })
   }
   function handleCvcBlur(e) {
     dispatch({
       type: 'removed_focus_from_cvc',
-      value: e.target.value,
     })
   }
 
   return (
-    <form
-      style={inlineStyle}
-      className={`${className} ${style.form}`}
-      onSubmit={handleSubmit}
-    >
+    <>
       <FormControl
         className={style.formControl}
         label="Cardholder Name"
-        status={
-          state.data.cardholderName.validation?.status
-        }
-        message={
-          state.data.cardholderName.validation?.message
-        }
+        status={data.cardholderName.validation?.status}
+        message={data.cardholderName.validation?.message}
         inputs={[
           {
             name: 'cardholderName',
             id: 'cardholderNameInput',
             placeholder: 'e.g. Jane Appleseed',
-            value: state.data.cardholderName.value,
+            value: data.cardholderName.value,
             onChange: handleCardholderNameChange,
             onBlur: handleCardholderNameBlur,
           },
@@ -135,9 +194,9 @@ function Form({ style: inlineStyle, className = '' }) {
         name="cardNumber"
         id="cardNumberInput"
         placeholder="e.g. 1234 5678 9123 0000"
-        value={state.data.cardNumber.value}
-        status={state.data.cardNumber.validation?.status}
-        message={state.data.cardNumber.validation?.message}
+        value={data.cardNumber.value}
+        status={data.cardNumber.validation?.status}
+        message={data.cardNumber.validation?.message}
         dispatch={dispatch}
         onBlur={handleCardNumberBlur}
       />
@@ -145,23 +204,19 @@ function Form({ style: inlineStyle, className = '' }) {
         <FormControl
           className={`${style.formControl} ${style.formControlColumn}`}
           label="Exp. Date (MM/YY)"
-          message={
-            state.data.cardExpDate.validation?.message
-          }
+          message={data.cardExpDate.validation?.message}
           status={
-            state.data.cardExpDate.validation?.status
-              .month ||
-            state.data.cardExpDate.validation?.status.year
+            data.cardExpDate.validation?.status.month ||
+            data.cardExpDate.validation?.status.year
           }
           inputs={[
             {
               name: 'cardExpDateMonth',
               id: 'cardExpDateMonthInput',
               placeholder: 'MM',
-              value: state.data.cardExpDate.value.month,
+              value: data.cardExpDate.value.month,
               status:
-                state.data.cardExpDate.validation?.status
-                  .month,
+                data.cardExpDate.validation?.status.month,
               onChange: handleExpDateMonthChange,
               onBlur: handleExpDateMonthBlur,
             },
@@ -169,10 +224,9 @@ function Form({ style: inlineStyle, className = '' }) {
               name: 'cardExpDateYear',
               id: 'cardExpDateYearInput',
               placeholder: 'YY',
-              value: state.data.cardExpDate.value.year,
+              value: data.cardExpDate.value.year,
               status:
-                state.data.cardExpDate.validation?.status
-                  .year,
+                data.cardExpDate.validation?.status.year,
               onChange: handleExpDateYearChange,
               onBlur: handleExpDateYearBlur,
             },
@@ -181,28 +235,62 @@ function Form({ style: inlineStyle, className = '' }) {
         <FormControl
           className={`${style.formControl} ${style.formControlColumn}`}
           label="CVC"
-          status={state.data.cardCvc.validation?.status}
-          message={state.data.cardCvc.validation?.message}
+          status={data.cardCvc.validation?.status}
+          message={data.cardCvc.validation?.message}
           inputs={[
             {
               name: 'cardCvc',
               id: 'cardCvcInput',
               placeholder: 'e.g. 123',
-              value: state.data.cardCvc.value,
+              value: data.cardCvc.value,
               onChange: handleCvcChange,
               onBlur: handleCvcBlur,
             },
           ]}
         />
       </>
-      <Button
-        type="submit"
-        className={style.button}
-        text="Confirm"
-        // onClick={handleFormSubmit}
-      />
-    </form>
+    </>
+  )
+})
+
+/* -------------------------------------------------------------------------- */
+/*                                    ICONS                                   */
+/* -------------------------------------------------------------------------- */
+
+function LoadingIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      style={{
+        margin: 'auto',
+        background: '',
+        display: 'block',
+        shapeRendering: 'auto',
+      }}
+      width="18px"
+      height="18px"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx={50}
+        cy={50}
+        fill="none"
+        stroke="#ffffff"
+        strokeWidth={10}
+        r={35}
+        strokeDasharray="164.93361431346415 56.97787143782138"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        />
+      </circle>
+    </svg>
   )
 }
-
-export default Form
